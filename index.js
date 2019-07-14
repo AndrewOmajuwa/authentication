7cad75388ec0fabb285a2105304dbab092e6fcfa
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-07-14 13:20:04 +0200

    Modified start script in package json

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.get('/test', (req, res) => {
-    res.send({
-        message: 'welcome to the app'
-    })
-})
+// app.get('/test', (req, res) => {
+//     res.send({
+//         message: 'welcome to the app'
+//     })
+// })
 
 
 app.listen(port, () => {
